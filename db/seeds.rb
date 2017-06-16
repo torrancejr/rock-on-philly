ce7e8bb47196f909fb281d0249a5b95c3df35baf
Meaghan Conroy <mconroy734@gmail.com>
2017-06-16 11:47:04 -0400

    more api work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
-
-def key
-  ENV["SEATGEEK_SECRET"]
-end
-def client_id
-  ENV["SEATGEEK_ID"]
-end
-response = RestClient.get("https://api.seatgeek.com/2/venues?city=Philadelphia&client_id=#{client_id}client_secret=#{key}")
+response = RestClient.get "https://api.yelp.com/v3/businesses/search?location=Philadelphia&categories=musicvenues", { Authorization: ENV['YELP_TOKEN'] }
 
 venues = JSON.parse(response.to_s)
 venues.each do |venue|
   Venue.create(
     name: venue["name"],
-    location: venue["address"],
-    website: venue["url"]
+    location: venue["location"]["address1"],
+    website: venue["url"],
+    photo: venue["image_url"]
   )
 end
