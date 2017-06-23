6b5281ffc5a66fa41b24ee69dc1f8a6c51234cbe
Meaghan Conroy <mconroy734@gmail.com>
2017-06-23 10:37:04 -0400

    heroku stuff

--- react/src/components/VenuesList.js
+++ react/src/components/VenuesList.js
@@ -37,7 +37,7 @@ class VenuesList extends Component {
   };
 
   getData() {
-    fetch('http://localhost:3000/api/v1/venues.json',
+    fetch('https://rock-on-philly.herokuapp.com/api/v1/venues.json',
       { mode: 'no-cors' }
     )
       .then(response => {
